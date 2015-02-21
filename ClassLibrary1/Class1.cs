1d4cd87ced75cae8f654379c1fdb1b6ed0b4b143
Tim Andersen <timander@gmail.com>
2015-02-20 21:41:50 -0600

    first time coding in C#

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        internal String Convert(int arabic)
+        {
+            String roman = "";
+            if (arabic == 0) return roman;
+            var highestRoman = RomanNumeralLookup().Where(kvp => arabic >= kvp.Key).First();
+            return roman + highestRoman.Value + Convert(arabic - highestRoman.Key);
+        }
+
+        private static Dictionary<int, string> RomanNumeralLookup()
+        {
+            var dic = new Dictionary<int, String>();
+            dic.Add(1000, "M");
+            dic.Add(900, "CM");
+            dic.Add(500, "D");
+            dic.Add(400, "CD");
+            dic.Add(100, "C");
+            dic.Add(90, "XC");
+            dic.Add(50, "L");
+            dic.Add(40, "XL");
+            dic.Add(10, "X");
+            dic.Add(9, "IX");
+            dic.Add(5, "V");
+            dic.Add(4, "IV");
+            dic.Add(1, "I");
+            return dic;
+        }
+
+    }
+}
---
+++ ClassLibrary1/RomanNumeralConverterTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class RomanNumeralConverterTest
+    {
+
+        [Test]
+        public void ConvertArabicToRoman()
+        {
+            AssertRoman("", 0);
+            AssertRoman("I", 1);
+            AssertRoman("II", 2);
+            AssertRoman("III", 3);
+            AssertRoman("IV", 4);
+            AssertRoman("V", 5);
+            AssertRoman("VI", 6);
+            AssertRoman("VII", 7);
+            AssertRoman("VIII", 8);
+            AssertRoman("IX", 9);
+            AssertRoman("X", 10);
+            AssertRoman("XI", 11);
+            AssertRoman("XL", 40);
+            AssertRoman("L", 50);
+            AssertRoman("XC", 90);
+            AssertRoman("C", 100);
+            AssertRoman("CD", 400);
+            AssertRoman("D", 500);
+            AssertRoman("CM", 900);
+            AssertRoman("M", 1000);
+
+
+        }
+
+        private static void AssertRoman(String roman, int arabic)
+        {
+            Assert.AreEqual(roman, new RomanNumeralConverter().Convert(arabic));
+        }
+
+    }
+}
